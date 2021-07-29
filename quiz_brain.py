ce3806028b490373a8daded491591d3ec14c925f
akhilmishra365 <88160249+akhilmishra365@users.noreply.github.com>
2021-07-29 23:48:26 +0530

    Add files via upload

---
+++ quiz_brain.py
@@ -0,0 +1,21 @@
+
+
+from data import question_data
+class QuizBrain :
+
+    def __init__(self , q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+    def still_has_question(self):
+        if self.question_number < len(self.question_list) :
+            return True
+        else:
+            return False
+
+
+    def new_question(self):
+        current_question=self.question_list[self.question_number]
+        self.question_number +=1
+        user = input(f"Q.{self.question_number}: {current_question.text} (True/False)")
+
